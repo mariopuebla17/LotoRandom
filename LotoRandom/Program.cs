441f4bb7578aa649218eb97c0119a8075911144e
Mario Puebla <mariopuebla17@gmail.com>
2018-08-05 23:51:50 -0300

    init

--- LotoRandom/Program.cs
+++ LotoRandom/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LotoRandom
 {
@@ -10,6 +7,24 @@ namespace LotoRandom
     {
         static void Main(string[] args)
         {
+            int qtd = 25;
+            Random rd = new Random();
+            List<int> numeros = new List<int>();
+            int number = 1;
+            for (int i = 1; i <= qtd; i++)
+            {
+                number = rd.Next(1, qtd + 1);
+                while (numeros.Contains(number))
+                {
+                    if (numeros.Count >= qtd + 1) // quando ficar em loop infinito vou quebrar o laço                        
+                        break;
+                    else
+                        number = rd.Next(1, qtd + 1);
+                }
+                numeros.Add(number);
+                Console.WriteLine(number.ToString());
+            }
+            Console.ReadLine();
         }
     }
 }
